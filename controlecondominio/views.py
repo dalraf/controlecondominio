97b301770f6034d8afa577c3f78973958e191661
Daniel Fonseca Alves <dalraf@gmail.com>
2018-11-15 16:26:33 -0200

    Feito controle de url

--- controlecondominio/views.py
+++ controlecondominio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.http import HttpResponse
+import datetime
+
+def index(request):
+    html = "<html><body>Hello World</body></html>"
+    return HttpResponse(html)
