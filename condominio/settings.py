5646c4a48e95ab80c3a62f33b6ee9b3545d2b866
Daniel Fonseca Alves <dalraf@gmail.com>
2018-11-28 16:38:11 -0200

    Adicionado app reset migrations

--- condominio/settings.py
+++ condominio/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'controlecondominio',
-    'bootstrap3'
+    'bootstrap3',
+    'reset_migrations'
 ]
 
 MIDDLEWARE = [
